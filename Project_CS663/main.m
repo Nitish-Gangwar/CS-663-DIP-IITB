d30875df2dc8026c34f9f523fe7edf5fddabf6ce
mikhilg10 <71340075+mikhilg10@users.noreply.github.com>
2021-11-22 18:02:28 +0530

    final denoise main

--- Project_CS663/main.m
+++ Project_CS663/main.m
@@ -1,21 +1,23 @@
 clc;clear;
-im_noflash = imread("potsdetail_01_noflash.jpg");
-im_flash = imread("potsdetail_00_flash.jpg");
-sigma_d = 3;
-sigma_r = 5;
+im_noflash = imread("cave01_01_noflash.jpg");
+im_flash = imread("cave01_00_flash.jpg"); 
+figure(1);subplot(1,2,1); imshow(im_noflash);title("No Flash");subplot(1,2,2); imshow(im_flash);title("flash");
+sigma_d = 2;
+sigma_r = 0.08;
 thr_shadow = 0.03;
 fprintf("Abase Bilateral \n ");
 Abase = bilateral_Abase(im_noflash,sigma_d,sigma_r);
-figure(1);
-subplot(1,2,1); imshow(im_noflash);
-subplot(1,2,2);imshow(Abase) ; title("Abase ImNF bilateral");
+figure(2);
+%subplot(1,2,1); imshow(im_noflash);
+subplot(1,2,1);imshow(Abase) ; title("Abase ImNF bilateral");
 %%%
 fprintf("\n Anr JointBilateral \n ");
 Anr = joint_bilateral_Anr(im_noflash,im_flash,sigma_d,sigma_r);
-figure(2);
-subplot(1,3,1); imshow(im_noflash);
-subplot(1,3,2); imshow(im_flash);
-subplot(1,3,3);imshow(Abase) ; title("Anr JBilateral");
+subplot(1,2,2);imshow(Abase) ; title("Anr JBilateral")
+%figure(2)
+%subplot(1,3,1); imshow(im_noflash);
+%subplot(1,3,2); imshow(im_flash);
+%subplot(1,3,3);imshow(Abase) ; title("Anr JBilateral");
 %%
 fprintf("\n Calculating Fdetail \n ");
 Fdetail = fdetail_transfer(im_flash ,sigma_d,sigma_r);
@@ -29,8 +31,10 @@ figure(4);imshow(M); title("detected Flash Shadows and Specularities");
 fprintf("\n Denoised image claculation \n ");
 Afinal = (1 - M).*(Anr.*Fdetail) + M.*Abase;
 figure(5);imshow(Afinal); title("A_{Final}");
-
-
+%%
+figure(6);
+subplot(1,3,1);imshow(im_noflash);title("NF");subplot(1,3,2);imshow(im_flash);title("Fl");subplot(1,3,3);imshow(Afinal); title("A_{Final}");
+%%
 function maskff = xshadow_flash_mask(im_nf,im_f,thr_shadow)
 sigma_blurr = 3 ;
 linA = rgb2gray(im_nf);
