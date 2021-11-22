363ec6c8661fa6887c251a6de3e0ef748fbd07d3
mikhilg10 <71340075+mikhilg10@users.noreply.github.com>
2021-11-22 18:17:45 +0530

    Add files via upload

--- Project_CS663/main.m
+++ Project_CS663/main.m
@@ -2,22 +2,17 @@ clc;clear;
 im_noflash = imread("cave01_01_noflash.jpg");
 im_flash = imread("cave01_00_flash.jpg"); 
 figure(1);subplot(1,2,1); imshow(im_noflash);title("No Flash");subplot(1,2,2); imshow(im_flash);title("flash");
-sigma_d = 2;
-sigma_r = 0.08;
+sigma_d = 2; %filer range required upto 6to24 pixels therefore -2*3 to 2*3
+sigma_r = 0.08; %recomended value 0.05 to 0.1 we have taken middle value approx
 thr_shadow = 0.03;
 fprintf("Abase Bilateral \n ");
 Abase = bilateral_Abase(im_noflash,sigma_d,sigma_r);
 figure(2);
-%subplot(1,2,1); imshow(im_noflash);
 subplot(1,2,1);imshow(Abase) ; title("Abase ImNF bilateral");
 %%%
 fprintf("\n Anr JointBilateral \n ");
 Anr = joint_bilateral_Anr(im_noflash,im_flash,sigma_d,sigma_r);
 subplot(1,2,2);imshow(Abase) ; title("Anr JBilateral")
-%figure(2)
-%subplot(1,3,1); imshow(im_noflash);
-%subplot(1,3,2); imshow(im_flash);
-%subplot(1,3,3);imshow(Abase) ; title("Anr JBilateral");
 %%
 fprintf("\n Calculating Fdetail \n ");
 Fdetail = fdetail_transfer(im_flash ,sigma_d,sigma_r);
